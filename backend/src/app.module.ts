c0f8955d0f22fa00e240311e432f7a126d3607eb
Szymon_Jankowski <petergrynn@gmail.com>
2019-11-13 17:50:21 +0100

    connect to mongo altas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,11 @@ import { ImagesService } from './images/images.service';
 import { ImagesController } from './images/images.controller';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config/keys';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true})],
   controllers: [AppController, ImagesController, UsersController],
   providers: [AppService, ImagesService, UsersService]
 })
--- backend/src/images/images.controller.ts
+++ backend/src/images/images.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('images')
-export class ImagesController {}
+export class ImagesController {
+    @Get()
+    findAll(): string{
+        return 'You are in images api';
+    }
+    @Get(':id')
+    findOne(@Param() param): string{
+        return `images nr: ${param.id}`;
+    }
+}
