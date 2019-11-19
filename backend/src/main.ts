0fb7dbb8231303b37ffb566685091cb187f2aa56
Szymon_Jankowski <petergrynn@gmail.com>
2019-11-19 19:00:07 +0100

    add security

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 10, // limit each IP to 100 requests per windowMs
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();
