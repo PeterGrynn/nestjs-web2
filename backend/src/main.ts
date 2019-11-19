71138f7cd56c66b0ec806393113bbeed707e87ae
Szymon_Jankowski <petergrynn@gmail.com>
2019-11-19 23:35:55 +0100

    add package.json

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,6 @@ import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
 import * as compression from 'compression';
 
-const likesLimit = rateLimit({
-  windowMs: 1000 * 60 * 60 * 24,
-  max: 2,
-});
 
 
 async function bootstrap() {
@@ -15,7 +11,7 @@ async function bootstrap() {
   app.enableCors();
   app.use(helmet());
   app.use(compression());
-  app.use('/comments/score/:id', likesLimit);
+  app.use('/comments/score/:id/plus', likesLimit);
   await app.listen(5000);
 }
 bootstrap();
